9735bd2661033f2ee0f0f332b457acd27151df5f
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-04-13 14:49:23 +0200

    Map renders successfully

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-require('dotenv').config()
 import React from 'react';
 import ReactDOM from 'react-dom';
 import mapboxgl from 'mapbox-gl';
-mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN;
+require('dotenv').config()
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
 
 class Application extends React.Component {
     constructor(props) {
@@ -20,6 +22,25 @@ class Application extends React.Component {
             center: [this.state.lng, this.state.lat],
             zoom: this.state.zoom
             });
+
+            map.on('move', () => {
+                this.setState({
+                  lng: map.getCenter().lng.toFixed(4),
+                  lat: map.getCenter().lat.toFixed(4),
+                  zoom: map.getZoom().toFixed(2)
+                });
+              });
+        }
+
+    render() {
+        return (
+            <div>
+                <div className='sidebarStyle'>
+                    <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
+                </div>
+                <div ref={el => this.mapContainer = el} className='mapContainer' />
+                </div>
+            )
         }
   }
   
